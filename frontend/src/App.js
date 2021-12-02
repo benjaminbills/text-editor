d208725b7e600385f308e1b57e97077d10e7c2dc
benjaminbills <benjaminoba234@gmail.com>
2021-12-02 20:38:19 +0300

    added react-quill

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 function App() {
+  const [value, setValue] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactQuill theme="snow" value={value} onChange={setValue} />
     </div>
   );
 }
